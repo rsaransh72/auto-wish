f819b49e05ea0a1f2ab6ef3f33b82ff138345731
Saransh Rai <rsaransh.72@gmail.com>
2024-05-30 18:54:07 +0000

    feature/AW-2 | installed mongoose

--- vbn/src/app.ts
+++ vbn/src/app.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import morgan from 'morgan';
-import helmet from 'helmet';
 import cors from 'cors';
 
 import * as middlewares from './middlewares';
@@ -12,7 +11,6 @@ require('dotenv').config();
 const app = express();
 
 app.use(morgan('dev'));
-app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
